6ce1ffe1a3abd903c06af3d482fad575bbe4eae0
Tung Nguyen <tungnc3142@gmail.com>
2021-11-10 01:31:49 +0700

    init

---
+++ layer.py
@@ -0,0 +1,65 @@
+import numpy as np
+class Layer:
+
+    def __init__(self):
+        pass
+
+    def forward(self, input):
+        return input
+
+    def backward(self, input, grad_output):
+        num_units = input.shape[1]
+
+        d_layer_d_input = np.eye(num_units)
+
+        return np.dot(grad_output, d_layer_d_input)  # chain rule
+
+class ReLU(Layer):
+    def __init__(self):
+        pass
+    
+    def forward(self, input):
+        relu_forward = np.maximum(0,input)
+        return relu_forward
+    
+    def backward(self, input, grad_output):
+        relu_grad = input > 0
+        return grad_output*relu_grad
+
+
+class Dense(Layer):
+    def __init__(self, input_units, output_units, learning_rate=0.1):
+        # A dense layer is a layer which performs a learned affine transformation:
+        # f(x) = <W*x> + b
+        
+        self.learning_rate = learning_rate
+        self.weights = np.random.normal(loc=0.0, 
+                                        scale = np.sqrt(2/(input_units+output_units)), 
+                                        size = (input_units,output_units))
+        self.biases = np.zeros(output_units)
+        
+    def forward(self,input):
+        # Perform an affine transformation:
+        # f(x) = <W*x> + b
+        
+        # input shape: [batch, input_units]
+        # output shape: [batch, output units]
+        
+        return np.dot(input,self.weights) + self.biases
+    
+    def backward(self,input,grad_output):
+        # compute d f / d x = d f / d dense * d dense / d x
+        # where d dense/ d x = weights transposed
+        grad_input = np.dot(grad_output, self.weights.T)
+        
+        # compute gradient w.r.t. weights and biases
+        grad_weights = np.dot(input.T, grad_output)
+        grad_biases = grad_output.mean(axis=0)*input.shape[0]
+        
+        assert grad_weights.shape == self.weights.shape and grad_biases.shape == self.biases.shape
+        
+        # Here we perform a stochastic gradient descent step. 
+        self.weights = self.weights - self.learning_rate * grad_weights
+        self.biases = self.biases - self.learning_rate * grad_biases
+        
+        return grad_input
